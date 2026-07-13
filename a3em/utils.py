066e1901ba87429e7f240a54f01867ec1001a458
Gabriel Barnard <gbarnard08@gmail.com>
2026-07-13 11:09:08 -0500

    got rid of stem helper

--- a3em/utils.py
+++ a3em/utils.py
@@ -66,11 +66,3 @@ def extract_features(audio: np.ndarray, sample_rate: int) -> float:
         **mfcc_dict,
         **mel_dict
     }
-
-
-def __get_path_stem(path: str) -> str:
-    split_char = '/' if os.name == 'posix' else '\\'
-    directories = path.split(split_char)
-    file_name = directories[-1]
-    parts = file_name.split('.')
-    return parts[0]
